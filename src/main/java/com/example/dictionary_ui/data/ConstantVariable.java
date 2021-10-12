397cb497140f69bfa3812fee674f080bd3336313
NTMinhh <hsgsbio.2k2@gmail.com>
2021-10-12 20:35:01 +0700

    online + resize

--- src/main/java/com/example/dictionary_ui/data/ConstantVariable.java
+++ src/main/java/com/example/dictionary_ui/data/ConstantVariable.java
@@ -2,6 +2,6 @@ package com.example.dictionary_ui.data;
 
 public class ConstantVariable {
     public static final String PATH = "src/main/java/com/example/dictionary_ui/data/dictionary.txt";
-    public static final int stage_width = 1300;
+    public static final int stage_width = 1400;
     public static final int stage_height = 839;
 }
